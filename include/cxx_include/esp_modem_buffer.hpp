f9cf60278e448f0e4e916decc02917d40f14529b
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-05-23 10:33:40 +0200

    initial commit

---
+++ include/cxx_include/esp_modem_buffer.hpp
@@ -0,0 +1,53 @@
+/*
+ * SPDX-FileCopyrightText: 2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+#include <cstring>
+
+namespace esp_modem {
+
+/**
+ * Common unique buffer, which is transferable between DTE and CMUX
+ *
+ */
+struct unique_buffer {
+    explicit unique_buffer(size_t size);
+    unique_buffer (unique_buffer const &) = delete;
+    unique_buffer &operator=(unique_buffer const &) = delete;
+    unique_buffer(unique_buffer &&other) noexcept
+    {
+        data = std::move(other.data);
+        size = other.size;
+        consumed = 0;
+    }
+    unique_buffer &operator=(unique_buffer &&other) noexcept
+    {
+        if (&other == this) {
+            return *this;
+        }
+        data = std::move(other.data);
+        size = other.size;
+        consumed = 0;
+        return *this;
+    }
+    [[nodiscard]] uint8_t *get() const
+    {
+        return data.get();
+    }
+
+    void wrap()
+    {
+        memmove(data.get(), data.get() + consumed, size - consumed);
+        size -= consumed;
+        consumed = 0;
+    }
+
+    std::unique_ptr<uint8_t[]> data;
+    size_t size{};
+    size_t consumed{};
+};
+
+}
